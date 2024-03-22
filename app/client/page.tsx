765224f819bccdbbb541f7f3ffb7ff225d589f98
Artiana <hei.rojo.3@gmail.com>
2024-03-22 22:09:26 +0300

    feat:update

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -29,40 +29,45 @@ const Pokedex = () => {
   }, [offset]);
 
   return (
-    <div className="container mx-auto px-4 py-10 sm:px-6 sm:py-24">
-      <h1 className="text-4xl font-bold mb-30 text-center">List of Pokemon</h1> 
-      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
-        {pokemonList.map((pokemon) => (
-          <div
-            key={pokemon.name}
-            className="flex flex-col items-center justify-center bg-blue-400 p-6 rounded-md"
-
+ <div className="container mx-auto px-4 py-10 sm:px-6 sm:py-24 bg-gradient-to-r from-blue-400 to-purple-500">
+    <h1 className="text-4xl font-bold mb-10 text-center text-white">List of Pokemon</h1>
+    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
+      {pokemonList.map((pokemon) => (
+        <div
+          key={pokemon.name}
+          className="flex flex-col items-center justify-center bg-white p-6 rounded-lg shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105"
+        >
+          <Image
+            src={`https://img.pokemondb.net/sprites/home/normal/${pokemon.name}.png`}
+            alt={pokemon.name}
+            width={96}
+            height={96}
+            className="rounded-full animate-spin-slow" 
+          />
+          <h2 className="text-center mb-4 text-2xl font-bold text-blue-500">{pokemon.name}</h2>
+          <button
+            onClick={() =>
+              router.push(
+                `/${pathname?.split("/")[1]}/${pokemon.name}`
+              )
+            }
+            className="mt-4 px-6 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300"
           >
-            <Image
-              src={`https://img.pokemondb.net/sprites/home/normal/${pokemon.name}.png`}
-              alt={pokemon.name}
-              width={96}
-              height={96}
-            />
-
-            <h2>{pokemon.name}</h2>
-
-            <button
-              onClick={() =>
-                router.push(
-                  `/${pathname?.split("/")[1]}/${pokemon.name}`
-                )
-              }
-              className="mt-4 px-6 py-1 bg-blue-200 text-white rounded-md"
-            >
-              Détails
-            </button>
-          </div>
-        ))}
-      </div>
-      <button onClick={() => setOffset(offset + 50)}>Suivant</button>
+            Détails
+          </button>
+        </div>
+      ))}
     </div>
-  );
+    <button
+      onClick={() => setOffset(offset + 50)}
+      className="mt-10 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300"
+    >
+      Suivant
+    </button>
+ </div>
+);
+
+   
 };
 
 export default Pokedex;
